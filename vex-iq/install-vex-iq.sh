1f036f2c64e1e01bebb2d04f6e6031c1cdf93138
Jiří Kvant Kubík <jiri.kub@gmail.com>
2023-10-30 22:22:24 +0100

    [veq-iq] making the install script more compact.

--- vex-iq/install-vex-iq.sh
+++ vex-iq/install-vex-iq.sh
@@ -3,13 +3,10 @@
 # INSTALL CHROME
 # name
 name="google-chrome-stable_current_amd64.deb"
-
 # remove if present
 rm -f $name
-
 # download
 wget https://dl.google.com/linux/direct/$name
-
 # install
 sudo dpkg -i ./$name
 
@@ -17,14 +14,9 @@ sudo dpkg -i ./$name
 # INSTALL ROBOT MESH CONNECT
 # name
 name="robotmeshconnect-0.6_amd64.deb"
-
 # remove if present
 rm -f $name
-
 # download
 wget https://www.robotmesh.com/downloads/robotmeshconnect-0.6_amd64.deb
-
 # install
 sudo dpkg -i ./$name
-
-
